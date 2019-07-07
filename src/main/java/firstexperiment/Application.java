0fe9a15c392d48e149f0e6f2cd270f059056ecdf
Kris Labs <Krislabs@Krishnas-MacBook-Pro-2.local>
2019-07-07 14:10:47 -0700

    first commit

---
+++ src/main/java/firstexperiment/Application.java
@@ -0,0 +1,12 @@
+package firstexperiment;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+
+
+    }
+}
