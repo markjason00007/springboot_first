af1a58059ba060d6f5957d27ddd5172d1f713d7d
Krishna Mohan <kris.moh725725@gmail.com>
2020-02-15 20:07:51 -0800

    adding the ArrayDemo file

---
+++ src/main/java/firstexperiment/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[] arr=new int[50];
+		
+		for(int i=0;i<=10;i++) {
+			arr[i] =i;
+		}
+		
+		System.out.println(" arr.lenth "+ arr.length);
+		
+	}
+
+}
